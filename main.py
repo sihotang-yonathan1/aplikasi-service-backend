baca4971de0d59743061b4689420e551d65ac9ec
Yonathan Sihotang <yonathansihotang026@student.unsrat.ac.id>
2024-12-13 06:31:56 +0800

    [main] Add CORS setting

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import os
 
 from dotenv import load_dotenv
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
 import mariadb
 
 from model.task import Task, TaskCreateRequestBody
@@ -50,6 +52,30 @@ async def lifespan(app: FastAPI):
     conn.close()
 
 app = FastAPI(lifespan=lifespan)
+
+
+# CORS Setting
+# WARNING: this may not safe, probably need to specify the origin
+origins = ['*']
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+# CORS Setting
+# WARNING: this may not safe, probably need to specify the origin
+origins = ['*']
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get('/todo')
